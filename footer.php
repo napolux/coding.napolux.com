8406a479869af5bf1d32c0b3b6ec05c32abe911a
Napolux <napolux@gmail.com>
2018-06-02 14:39:24 +0200

    .gitignore is now working

---
+++ footer.php
@@ -0,0 +1,29 @@
+</div><!-- container -->
+<footer id="footer" class="footer sans" role="contentinfo">
+    <div class="footer__top">
+        <div class="footer__top-content content">
+            <h6 class="sans footer__top-content-intro">MORE WORDS...</h6>
+            <?php
+                if (function_exists('get_random_post')) {
+                    get_random_post();
+                } else {
+                    echo 'Please check your functions.php file.';
+                }
+            ?>
+        </div>
+    </div>
+    <div class="footer__bottom">
+        <div class="footer__bottom-content content">
+            <h2>About me</h2>
+            <p>I'm Francesco, a programmer. Somebody call me <strong>Napo</strong> or <strong>Napolux</strong>.
+            This is my blog about coding. You can find all the small projects I work on in my spare time here. </p>
+            <p>If you like these tutorials or you think they're useful, <a title="Make me a 3.99€ donation using PayPal" href="https://www.paypal.me/napolux/3.99">you can buy me a coffee using PayPal</a>.</p>
+            <p>Please, read the license: <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY-NC-SA</a>.
+            Made with <a href="https://wordpress.org" title="WordPress">WordPress</a>.</p>
+        </div>
+    </div>
+</footer>
+</div><!-- wrapper -->
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>"/>
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<div id="wrapper" class="hfeed">
+    <header id="header" class="header serif" role="banner">
+        <div class="header__content content">
+            <?php if(is_home()) { ?>
+                <h1><?php echo esc_html(get_bloginfo('name')); ?></h1>
+                <p><?php bloginfo('description'); ?></p>
+            <?php } else { ?>
+                <h2><?php echo esc_html(get_bloginfo('name')); ?></h2>
+                <p><?php bloginfo('description'); ?></p>
+            <?php } ?>
+        </div>
+    </header>
+    <div id="container">
